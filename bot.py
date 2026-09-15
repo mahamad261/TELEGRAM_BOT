1830e3d4b93bcda577cb50906f6262bfc7ce650d
mahamad261 <res159071@gmail.com>
2026-09-15 18:42:58 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -32,7 +32,7 @@ BOT_TOKEN = os.getenv("BOT_TOKEN", "").strip()
 try:
     ADMIN_ID = int(os.getenv("ADMIN_ID", "0").strip() or "0")
 except ValueError:
-    ADMIN_ID = 0
+    ADMIN_ID = 7831049189
 
 DB_FILE = "running_db.json"
 
